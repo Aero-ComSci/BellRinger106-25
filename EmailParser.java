2edf9c821bfc03736ff23521553add1e535a4999
Matius980 <157426874+Matius980@users.noreply.github.com>
2025-10-06 13:56:49 -0700

    Create EmailParser.java
    code

---
+++ EmailParser.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class EmailParser {
+    public static void main(String[] args) {
+        String sampleEmail = "bobby.jones567@gmail.com";
+        String user = sampleEmail.split("@")[0];
+        String[] parts = user.split("\\.");
+        String firstName = parts[0];
+        String lastName = parts[1];
+        System.out.println(firstName + " " + lastName);
+    }
+}
